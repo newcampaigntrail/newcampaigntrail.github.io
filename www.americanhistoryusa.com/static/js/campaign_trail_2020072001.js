3548ef060590cc96f0b67ebe7fcb2ab1986f9653
Danxv33 <63205469+Danxv33@users.noreply.github.com>
2022-01-15 18:13:10 -0500

    Update campaign_trail_2020072001.js

--- www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
+++ www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
@@ -27,6 +27,20 @@ function gradient(interval, min, max) {
 	}
 }
 
+function previewFile() {
+  const content = document.querySelector('.content');
+  const [file] = document.querySelector('input[type=file]').files;
+  const reader = new FileReader();
+
+  reader.addEventListener("load", () => {
+    // this will then display a text file
+    content.innerText = reader.result;
+  }, false);
+
+  if (file) {
+    reader.readAsText(file);
+  }
+}
 
 ////https://codepen.io/njmcode/pen/axoyD/
 
