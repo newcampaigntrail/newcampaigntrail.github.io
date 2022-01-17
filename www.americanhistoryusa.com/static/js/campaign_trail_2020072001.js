2dc2bf2fc5fa48414709cff97bd9135026e3eb2c
Danxv33 <63205469+Danxv33@users.noreply.github.com>
2022-01-16 20:01:13 -0500

    Update campaign_trail_2020072001.js

--- www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
+++ www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
@@ -496,7 +496,6 @@ e.player_visits=campaigntrail.player_visits
 e.final_overall_results=campaigntrail.overall_results
 e.final_state_results=campaigntrail.state_results
 e.difficulty_level_multiplier=campaigntrail.difficulty_multiplier
-starting_mult=campaigntrail.starting_mult	
 d()
 }		
 
