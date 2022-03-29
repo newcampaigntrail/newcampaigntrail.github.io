6451689410198aa4c387c7902162e04c7ae0f03a
Danxv33 <63205469+Danxv33@users.noreply.github.com>
2022-03-28 21:36:57 -0400

    Update campaign_trail_2020072001.js

--- www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
+++ www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
@@ -9,6 +9,7 @@ if (window.localStorage.getItem("margin_form") == "#C9C9C9") {
 }
 function creatorChange()
 {
+Alert("executed")	
 if(document.getElementById("election_id").value==69)
 {
 campaignTrail_temp.credits="T3CHOX"   
