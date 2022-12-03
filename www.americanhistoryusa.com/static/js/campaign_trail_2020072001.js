e4f3f4c3cbf2e2b06741c3db9fe46114a3968f8b
Decstar <fakeemail9424@gmail.com>
2022-12-03 10:04:49 +1000

    *Someone* forgot that mod achievements are allowed to be modded

--- www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
+++ www.americanhistoryusa.com/static/js/campaign_trail_2020072001.js
@@ -1673,13 +1673,13 @@ function divideElectoralVotesProp(e, t) {
                 // ### 2008 ###
 
                 // The People's President - Win as Comrade McCain!
-                if (!modded && !run.achievements["peoplesvictory"] && e.candidate_last_name == "McCain" && e.final_outcome=="win" && e.player_answers[8] == 52632 && e.election_id==20) {
+                if (!run.achievements["peoplesvictory"] && e.candidate_last_name == "McCain" && e.final_outcome=="win" && e.player_answers[8] == 52632 && e.election_id==20) {
                     unlockAchievement(among, "peoplesvictory", "", "<b>The People's President</b>")
                 } else
                 // ### 1876 ####
                     
                 // A Truce, Not A Compromise
-                if (!modded && !run.achievements["ATruceNotACompromise"] && ((e.candidate_last_name == "Hayes" && e.final_overall_results[n].electoral_votes == 185 && e.player_answers[24] == 8090) || (e.candidate_last_name == "Tilden" && e.final_overall_results[n].electoral_votes == 184 && e.player_answers[24] == 8088))  && e.election_id==5) {
+                if (!run.achievements["ATruceNotACompromise"] && ((e.candidate_last_name == "Hayes" && e.final_overall_results[n].electoral_votes == 185 && e.player_answers[24] == 8090) || (e.candidate_last_name == "Tilden" && e.final_overall_results[n].electoral_votes == 184 && e.player_answers[24] == 8088))  && e.election_id==5) {
                     unlockAchievement(among, "ATruceNotACompromise", "", "<b>A Truce, Not A Compromise</b>")
                 }	
 
