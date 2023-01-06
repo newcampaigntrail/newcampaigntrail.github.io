9e6889f325d24b210c097bd0bb8ac7c29798461d
Danxv33 <63205469+Danxv33@users.noreply.github.com>
2023-01-05 19:46:24 -0500

    Update campaign_trail.js

--- www.americanhistoryusa.com/static/js/campaign_trail.js
+++ www.americanhistoryusa.com/static/js/campaign_trail.js
@@ -45,8 +45,8 @@ var fileExists = function(url) {
     return req.status === 200;
 };
 
-lastUpdatedDate="2022-12-10"
-var RecReading;
+lastUpdatedDate="2023-01-05"
+var RecReading
 
 achList = {
     "destiny": [
@@ -385,12 +385,7 @@ function openInfoMenu() {
     <div style='text-align:left'><p>- /r/thecampaigntrail<pr />
      <pr /> 
     <div style='text-align:left'><p>Most recent TNCT patch notes:<pr />
-    <div style='text-align:left'><p>This section you are reading was added, as well as the mod loader was updated to be more modular (no pun intended). We are aware of a glitch that has many people unable to use some mods now.</p>
-    <div style='text-align:left'><p>To fix this error, please clear your cache. It should work after that is done.</div>
-    <div style='text-align:left'><p>Additionally, a primaries feature for TNCT mods have been added. It is quite complicated, so if you wish to make a mod with it, please join our Discord server for help.</div>
-    <div style='text-align:left'><p>Further reading has been added. In order for it to work, follow this guide:</div>
-    <div style='text-align:left'><p>https://www.reddit.com/r/thecampaigntrail/comments/ziartb/further_reading_has_been_added/</div>
-    <div style='text-align:left'><b>Note: multiple achievements cannot be received at once.</b>`
+    <div style='text-align:left'><p>Happy new year!You can now share games through URLs, just like the old site. Additionally, a lot of small speed and performance fixes have been made.</p>
     
 	$("#infoBox").html(`
     <div class="inner_window_front" style="padding:0px"><b><h1>Welcome to The New Campaign Trail!</h1></b></div>
