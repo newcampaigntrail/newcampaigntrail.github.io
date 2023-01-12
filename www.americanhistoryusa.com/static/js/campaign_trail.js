9fb31f5ca5de462d721a7af99b8769ed737ee8c1
Astronomical <84736671+ItsAstronomical@users.noreply.github.com>
2023-01-12 17:54:01 -0500

    adding you will win oregon

--- www.americanhistoryusa.com/static/js/campaign_trail.js
+++ www.americanhistoryusa.com/static/js/campaign_trail.js
@@ -183,7 +183,7 @@ achList = {
     // Mods
     //2024 (Upon a Cross of Globalism)
     "youwillwinoregon": [
-        "You <b>WILL</b> win Oregon.",
+        "You <b>WILL</b> win Oregon",
         "As Tucker Carlson, win Oregon in 2024 (Upon a Cross of Globalism) while losing the election to Newsom.",
         "</table><br><h2>Mods</h2><br><b><em>2024G</em></b><br><table>"
     ],   
@@ -2394,6 +2394,12 @@ function divideElectoralVotesProp(e, t) {
 
                 // ###### MODS ######
 
+		// ### 2024G ###
+                // You WILL win Oregon
+                if (!run.achievements["youwillwinoregon"] && e.candidate_last_name == "Carlson" && e.election_id == 20 && campaignTrail_temp.difficulty_level_multiplier <= 0.97 && 23 == campaignTrail_temp.final_state_results[36].result[0].candidate=200 && e.final_outcome=="lose") {
+                    unlockAchievement(among, "youwillwinoregon", "", "You <b>WILL</b> win Oregon")
+                } else
+		
                 // ### 2019NK ###
                 // Potato P.R.I.D.E - Get the true ending to 2019NK.
                 if (!run.achievements["trueKorea"] && e.running_mate_last_name
