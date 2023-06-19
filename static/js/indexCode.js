a3b24668d131dfd58374db80d149deecd727fceb
Decstar <fakeemail9424@gmail.com>
2023-06-19 15:39:54 +1000

    bigshot input validation + better input confirmation

--- static/js/indexCode.js
+++ static/js/indexCode.js
@@ -791,8 +791,23 @@ minimizeBtn.addEventListener('click', () => {
     cheatMenu.classList.toggle('minimized');
 });
 
-$("#bigshotSkipBtn").click(() => {
-    campaignTrail_temp.question_number = Number(document.getElementById('skiptoquestion').value);
+$("#bigshotSkipBtn").click((e) => {
+    e.preventDefault();
+
+    let question_count = campaignTrail_temp.global_parameter_json[0].fields.question_count;
+    let requested_question = Number(document.getElementById('skiptoquestion').value);
+
+    // error handling in case the user is stupid
+    if (requested_question > question_count - 1) {
+        let a = confirm("You are trying to go over the question limit. This will likely break unless the mod maker specifically made questions for this (possibly for CYOA). Are you sure?");
+        if (!a) return
+    }
+    if (requested_question < 0) {
+        let a = confirm("You are trying to go below question 1. This will almost certainly break. Are you sure?");
+        if (!a) return
+    }
+
+    campaignTrail_temp.question_number = requested_question;
 
     let sfx = $('#sfxMeme')[0];
     sfx.volume = 0.1;
@@ -802,6 +817,8 @@ $("#bigshotSkipBtn").click(() => {
 duplicate_globals = {}
 
 $("#disableRNG").change((a) => {
+    a.preventDefault();
+
     let checkbox = a.target;
     let checked = checkbox.checked;
 
@@ -814,3 +831,10 @@ $("#disableRNG").change((a) => {
         campaignTrail_temp.global_parameter_json[0] = JSON.parse(JSON.stringify(duplicate_globals));
     }
 })
+
+$("#skiptoquestion").keydown((e) => {
+    if (e.key === 'Enter') {
+        e.preventDefault();
+        $("#bigshotSkipBtn").click();
+    }
+});
