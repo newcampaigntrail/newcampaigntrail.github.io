2a0f99548fdb31a73c594236c321d27639d35139
Decstar <fakeemail9424@gmail.com>
2023-06-23 19:36:42 +1000

    added background cover option

--- static/js/indexCode.js
+++ static/js/indexCode.js
@@ -563,12 +563,11 @@ function themePicked() {
 susnum = Math.floor((Math.random() * 8) + 1)
 stassennum = Math.floor((Math.random() * 8) + 1)
 stassenyear = ["1944", "1948", "1952", "1964", "1968", "1980", "1984", "1988", "1992"]
-nct_stuff.quotes = [`"All men are created equal" - Thomas Jefferson`, `"All the Way with LBJ!"`, `"Those who make peaceful revolution impossible, make violent revolution inevitable" - John Kennedy`, `"I'm Gerald Ford, and you're not."`, `"Fool me once, shame on  -  shame on you. Fool me  -  you can't get fooled again." - George W. Bush`, `"It's the economy stupid!"`, `"You shall not crucify mankind upon a cross of gold." - William Jennings Bryan`, `"Politics have no relations to morals." - Machiavelli`, `"I am not going to exploit, for political purposes, my opponent's youth and inexperience." - Ronald Reagan`, `"There are weapons of mass destruction in Iraq." - George W. Bush`, `"That was really uncalled for, Senator." - Dan Quayle`, `"My failures have been error in judgement, not intent." - Ulysses S. Grant`, `"When the president does it, that means it is not illegal" - Richard Nixon`, `"Leave nothing for tomorrow which can be done today" - Abraham Lincoln`, `"Extremism, in the defense of liberty, is no vice." - Barry Goldwater`, `"My fellow Americans, I'm pleased to tell you today that I've signed legislation that will outlaw Russia forever. We begin bombing in five minutes." - Ronald Reagan`, `"Injustice anywhere, is a threat to justice everywhere." - Martin Luther King`, `"The Dream shall never die!" - Ted Kennedy`, `"I have binders full of women!" - Mitt Romney`, `"Who am I? Why am I here?" - James Stockdale`, `"Read My Lips: No New Taxes." - George HW. Bush`, `"Thomas Jefferson lives." - John Adams`, `"Tippecanoe and Tyler Too!"`, `"There is no Soviet domination of eastern Europe!" - Gerald Ford`, `"Change based on principle is progress. Constant change without principle becomes chaos." - Dwight Eisenhower`, `"Freedom is never more than one generation away from extinction."" - Ronald Reagan`, ` "Ladies and gentlemen, I don't know whether you fully understand that I have just been shot," - Theodore Roosevelt`, `"DEWEY DEFEATS TRUMAN"`, `"Hell no, we won't go!"`, '"Hey, hey, LBJ, how many kids did you kill today?"', `"Vote for the Crook: It’s Important!`, `"Every man a king, but no one wears a crown!"`, `"Keep cool with Coolidge!"`, `"Don't swap horses in the middle of the stream."`, `"Mr. Gorbachev, tear down this wall!" - Ronald Reagan`, `"Pokémon Go to the Polls!" - Hillary Clinton`, `"The only thing we have to fear is fear itself." - Franklin Roosevelt`, `"Only those who have the courage to fail greatly achieve greatly." - Robert Kennedy`, `"Amnesty, acid, and abortion."`, `"In your heart, you know he's right!"`, `"Give me liberty, or give me death!" - Patrick Henry`, `"Vote for Taft now, you can vote for Bryan anytime!"`, `"Vote for the crook. It's important."`, `"Blaine, Blaine, James G. Blaine, The Continental Liar from the State of Maine!"`, `"I like Ike!"`, `"A house divided can not stand." - Abraham Lincoln`, `"Ask not what your country can do for you- ask what you can do for your country." - John Kennedy`, `"Give me six hours to chop down a tree and I will spend four sharpening the axe" - Abraham Lincoln`]
+nct_stuff.quotes = [`"All men are created equal" - Thomas Jefferson`, `"All the Way with LBJ!"`, `"Those who make peaceful revolution impossible, make violent revolution inevitable" - John Kennedy`, `"I'm Gerald Ford, and you're not."`, `"Fool me once, shame on  -  shame on you. Fool me  -  you can't get fooled again." - George W. Bush`, `"It's the economy stupid!"`, `"You shall not crucify mankind upon a cross of gold." - William Jennings Bryan`, `"Politics have no relations to morals." - Machiavelli`, `"I am not going to exploit, for political purposes, my opponent's youth and inexperience." - Ronald Reagan`, `"There are weapons of mass destruction in Iraq." - George W. Bush`, `"That was really uncalled for, Senator." - Dan Quayle`, `"My failures have been error in judgement, not intent." - Ulysses S. Grant`, `"When the president does it, that means it is not illegal" - Richard Nixon`, `"Leave nothing for tomorrow which can be done today" - Abraham Lincoln`, `"Extremism, in the defense of liberty, is no vice." - Barry Goldwater`, `"I've signed legislation that will outlaw Russia forever. We begin bombing in five minutes." - Ronald Reagan`, `"Injustice anywhere, is a threat to justice everywhere." - Martin Luther King`, `"The Dream shall never die!" - Ted Kennedy`, `"I have binders full of women!" - Mitt Romney`, `"Who am I? Why am I here?" - James Stockdale`, `"Read My Lips: No New Taxes." - George HW. Bush`, `"Thomas Jefferson lives." - John Adams`, `"Tippecanoe and Tyler Too!"`, `"There is no Soviet domination of eastern Europe!" - Gerald Ford`, `"Change based on principle is progress. Constant change without principle becomes chaos." - Dwight Eisenhower`, `"Freedom is never more than one generation away from extinction."" - Ronald Reagan`, ` "Ladies and gentlemen, I don't know whether you fully understand that I have just been shot," - Theodore Roosevelt`, `"DEWEY DEFEATS TRUMAN"`, `"Hell no, we won't go!"`, '"Hey, hey, LBJ, how many kids did you kill today?"', `"Vote for the Crook: It’s Important!`, `"Every man a king, but no one wears a crown!"`, `"Keep cool with Coolidge!"`, `"Don't swap horses in the middle of the stream."`, `"Mr. Gorbachev, tear down this wall!" - Ronald Reagan`, `"Pokémon Go to the Polls!" - Hillary Clinton`, `"The only thing we have to fear is fear itself." - Franklin Roosevelt`, `"Only those who have the courage to fail greatly achieve greatly." - Robert Kennedy`, `"Amnesty, acid, and abortion."`, `"In your heart, you know he's right!"`, `"Give me liberty, or give me death!" - Patrick Henry`, `"Vote for Taft now, you can vote for Bryan anytime!"`, `"Vote for the crook. It's important."`, `"Blaine, Blaine, James G. Blaine, The Continental Liar from the State of Maine!"`, `"I like Ike!"`, `"A house divided can not stand." - Abraham Lincoln`, `"Ask not what your country can do for you- ask what you can do for your country." - John Kennedy`, `"Give me six hours to chop down a tree and I will spend four sharpening the axe" - Abraham Lincoln`]
     //nct_stuff.quotes = ["Remember, if your campaign manager isn't yelling at you, you're playing this game wrong.","Ralph Nader won in 2000","The American people are tired of women - Bernie Sanders","So I can just write anything here and it appears on the website?","Vote for President Johnson on November 3rd. The stakes are too high for you to stay home.","Dewey Defeats Truman!","You are not immune to 306-232","Read my lips. No new taxes","I did not have sexual relations with that woman","You know that your future still lies ahead of you.","Obligatory Wallave mention","if you don’t vote for me, ‘you ain’t black!","Fool me once, shame on...shame on you. Fool me—you can't get fooled again","We won this election by a lot!","Created by Al Gore under the pseudonym Dan Bryan","Senator, you’re no Dan Bryan","Yes, I would support the death penalty in that instance, Mr.Shaw","Making a mod; need a moder and a coder","We stan Dan Bryan in this household!","Most Americans wonder if you hit your head the wrong way getting out of bed this morning.","When he’s a tariff man through and through😩😩😩","Are you an Egghead or Lemonhead?","DO NOT LOOK UP WHERE HUBERT HUMPHREY WAS BORN","50 unban, 60% rual", "Look being a moderator, is all about learning from mistakes...", "Now with 30% less damn commies <font size='1'>McCarthy Approved</font>","I’ve never seen a skinny person drink Diet Coke","Alright. Way to get that crowd fired up.","<font size='1'>This is a good, liberal answer that will motivate your base. As always, make sure you strike the right balance between appealing to moderates and getting your troops out.</font>","Harold Stassen for president "+stassenyear[stassennum]+"!","Minecraft is better", "Statement by Donald J. Trump, 45th President of the United States of America","Now with an updated mod loader! (yes, seriously)", "Feels like a good time for a fireside chat", "[Insert Undertale Reference]", "2012 is the best scenario!", "Shopping for a New Deal", "Now with 10% more sarcastic comments", "<button onclick='alert(\"bepis\")'>Click for bepis</button>", "Don't stop me now! <font size='1'>no seriously please don't</font>", "she/they #BLM #ACAB", "The Industrial Revolution and its consequences have been a disaster for the human race", "Patriot | 80 years old | Unvaxxed | In ICU with COVID 😔", "<b>NEWER CAMPAIGN TRAIL</b> when?", "I'm all alone... more or less", "Let me fly far away from here", "<audio src='https://ia601807.us.archive.org/32/items/lp_fly-me-to-the-moon_earl-grant/disc1/01.02.%20Fly%20Me%20To%20The%20Moon%20%28In%20Other%20Words%29.mp3' autoplay='true'></audio>", "I like Ike!", "Giraffes are heartless creatures", "\"People who are hungry and out of a job are the stuff of which dictatorships are made.\" - Franklin D. Roosevelt", "\"The only thing we have to fear is fear itself.\" - Franklin D. Roosevelt", "\"You are fake news.\" - Donald J. Trump", "\"What counts is not necessarily the size of the dog in the fight - it's the size of the fight in the dog.\" - Dwight D. Eisenhower", "If you don't stand for something, you will fall for anything.", "Tiocfaidh ár lá", "https://www.twitch.tv/itsastronomics", "Wow, a cow made of butter. My girls would love it. In fact, the first sentence Caroline ever said was \"I like Butter.\" - Ted Cruz", "The plan is to fan this spark into a flame but damn, it's getting dark"]
     //nct_stuff.quotes = [`It's cold outside, no kind of atmosphere!`, `I'll be honest, I reused the Christmas code from last year.`, `Turles planted the Christmas Tree of Might!`, `"I am Champion Christmas" - Son Goku`, `Let me <em>flyyyy</em> far away from here`, `2019NK is just the start`, `Waiting for Sumner 68`, `"Cold of the winter, snow settles on my face" - Christmas Khamsin`, `Do you believe in gravity?`, `Who was the person who decided to makes quotes that overflow over the text width limit, y'know I specifically tried to avoid this when making the NCT quote section >:(`, `Denying Reagan his Christmas present since 1984`, `[insert unfunny community injoke here]`, `<button onclick='alert("Made mod.")'>Click here to make mod</button>`, `Merry Holidays.`, `Happy Christmas.`, `SONS OF LIBERTY - COMING OUT 2050`, `Florida has a date with the bottom of the ocean`, `Patch 1.02 - Removed Ohio`, `Enjoying our Charlie Cristmas`, `Ron Desanta - that's it, that's the quote.`]
 quotnum = Math.floor((Math.random() * nct_stuff.quotes.length))
 quote = nct_stuff.quotes[quotnum]
-
 
 // Caching frequently accessed elements and values
 let correctbannerpar = document.getElementsByClassName("game_header")[0]
@@ -636,38 +635,43 @@ function updateDynamicStyle() {
     if (nct_stuff.dynamicOverride) {
         return;
     }
+    let background_size_css = "";
+    if (selectedTheme.background_cover) {
+        background_size_css = "background-size: cover"
+    }
     let dynaStyle = `
     #header {
-      src: ${selectedTheme.banner};
-      width: 1000px;
+        src: ${selectedTheme.banner};
+        width: 1000px;
     }
     body {
-      background: ${selectedTheme.background};
+        background: ${selectedTheme.background};
+        ${background_size_css};
     }
     #game_window {
-      background-color: ${selectedTheme.coloring_window};
-      color: black;
-      background-image: ${selectedTheme.window_url ? "url(" + selectedTheme.window_url + ")" : $("#game_window")[0].style.backgroundImage};
+        background-color: ${selectedTheme.coloring_window};
+        color: black;
+        background-image: ${selectedTheme.window_url ? "url(" + selectedTheme.window_url + ")" : $("#game_window")[0].style.backgroundImage};
     }
     .container {
-      background-color: ${selectedTheme.coloring_container};
-      color: ${selectedTheme.text_col || "inherit"};
+        background-color: ${selectedTheme.coloring_container};
+        color: ${selectedTheme.text_col || "inherit"};
     }
     .game_header {
-      background-color: ${selectedTheme.coloring_title};
+        background-color: ${selectedTheme.coloring_title};
     }
     #inner_window_2 {
         background-color: ${selectedTheme.coloring_window};
-      }
-      #inner_window_3 {
+    }
+    #inner_window_3 {
         background-color: ${selectedTheme.coloring_window};
-      }
-      #inner_window_4 {
+    }
+    #inner_window_4 {
         background-color: ${selectedTheme.coloring_window};
-      }
-      #inner_window_5 {
+    }
+    #inner_window_5 {
         background-color: ${selectedTheme.coloring_window};
-      }  
+    }  
     #main_content_area {
         color: ${selectedTheme.text_col || "inherit"};
     }
@@ -680,10 +684,10 @@ function updateDynamicStyle() {
     #menu_container {
         color: black;
     }
-  `;
-  if (dynamicStyle.innerHTML != dynaStyle) {
-    dynamicStyle.innerHTML = dynaStyle;
-  }
+    `;
+    if (dynamicStyle.innerHTML != dynaStyle) {
+        dynamicStyle.innerHTML = dynaStyle;
+    }
 }
 
 // Update banner, styling, and game header on interval
@@ -822,6 +826,7 @@ open_first_gate = (e) => { // gate of opening
     <div class='prometh'>
     <h3>Custom Theme Menu</h3>
     <p>Background Image URL: <input id='background_url' placeholder='Link directly to the image.' /></p>
+    <p>Background Image Covers?: <input id='background_cover' type='checkbox' /></p>
     <p>Banner Image URL (suggested dimensions: 1000x303): <input id='banner_url' placeholder='Link directly to the image.' /></p>
     <p>Window Image URL (<b>OPTIONAL</b>, WILL LOOK BAD IF YOU DON'T KNOW WHAT YOU'RE DOING): <input id='window_url' placeholder='Link directly to the image.' /></p>
     <p>Window Colouring: <input id='window_colour' type='color' /></p>
@@ -837,6 +842,7 @@ open_first_gate = (e) => { // gate of opening
     if (th) {
         let theme = JSON.parse(th);
         $("#background_url").val(theme.background);
+        $("#background_cover")[0].checked = theme.background_cover;
         $("#banner_url").val(theme.banner);
         $("#window_url").val(theme.window_url)
         $("#window_colour").val(theme.coloring_window);
@@ -847,11 +853,12 @@ open_first_gate = (e) => { // gate of opening
     }
 
     $("#prometheus_button").off("click").click(open_fifth_gate);
-    
+
     $("#prometh_save").click(() => {
         let theme = {
             name: "Custom",
             background: $("#background_url").val(),
+            background_cover: $("#background_cover")[0].checked,
             banner: $("#banner_url").val(),
             coloring_window: $("#window_colour").val(),
             coloring_container: $("#cont_colour").val(),
@@ -881,7 +888,7 @@ open_fifth_gate = (e) => { // gate of closing
 
     // close menu
     let m_children = Array.from(menu_area.children);
-    m_children.forEach(f=>f.remove());
+    m_children.forEach(f => f.remove());
 
     $("#prometheus_button").off("click").click(open_first_gate);
 }
@@ -892,7 +899,7 @@ if (nct_stuff.selectedTheme == "custom") {
 
     let theme_man_button = document.createElement("p");
     theme_man_button.innerHTML = "<button id='prometheus_button'><b>Prometheus' Menu</b></button>";
-    
+
     themePicker.appendChild(theme_man_button);
 
     var prometheusStyle = document.createElement('style');
