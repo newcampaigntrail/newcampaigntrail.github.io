8e75af785d4324bd7dc8ecddd7d0bd80e7b56408
ItsAstronomical <84736671+ItsAstronomical@users.noreply.github.com>
2025-01-11 19:10:09 -0500

    Update indexCode.js

--- static/js/indexCode.js
+++ static/js/indexCode.js
@@ -502,7 +502,7 @@ nct_stuff.themes = {
         banner: "https://i.imgur.com/lXxBoP4.png",
         coloring_window: "#ffffff",
         coloring_container: "#326942",
-        coloring_title: "#326942"
+        coloring_title: "#326942",
         text_col: "white"
     },
     "custom": {
